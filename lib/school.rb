a4c2d0aa8db00a68174f562a911c3f4f9e85002d
Alie Stone <helloaliestone@gmail.com>
2020-09-26 14:30:43 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,10 +1,14 @@
 class School
-  attr_accessor :add_student, :roster
+  attr_accessor :add_student, :roster, :grade
   attr_reader :name
 
   def initialize(name)
     @name = name
     @roster = {}
+  end
+
+  def grade(grade)
+    return roster[grade] ? roster[grade]: []
   end
 
   def add_student(student, grade)
