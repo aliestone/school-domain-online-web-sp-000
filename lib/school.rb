773b060e8f8ca15e24e53ed18a3adc67fd275541
Alie Stone <helloaliestone@gmail.com>
2020-09-26 14:06:25 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,14 @@
-# code here!
+class School
+attr_accessor :add_student, :roster
+attr_reader :name
+
+def initialize(name)
+  @name = name
+  @roster = []
+end
+
+def add_student(student, grade)
+  roster << student
+end
+
+end
