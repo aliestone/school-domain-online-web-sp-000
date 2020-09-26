a6fec4b33551816e3fa00769d782fca4c8127cad
Alie Stone <helloaliestone@gmail.com>
2020-09-26 14:46:54 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -11,6 +11,12 @@ class School
     return roster[grade] ? roster[grade]: []
   end
 
+  def sort
+    roster.each do |grade, students|
+      students.sort!
+    end
+  end
+
   def add_student(student, grade)
     if roster[grade] == nil
       roster[grade]= []
